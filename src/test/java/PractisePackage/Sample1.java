c6676c688cafbf975006254437decd6c2f1ec9ec
sasikalagithub <sasikalaManthena28@gmail.com>
2022-11-22 13:13:12 +0530

    childBranch1

---
+++ src/test/java/PractisePackage/Sample1.java
@@ -0,0 +1,12 @@
+package PractisePackage;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+	@Test
+	public void demo()
+	{
+		System.out.println("sample class in Sridevi project");
+	}
+
+}
