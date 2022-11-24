9d88ea5020de43e4454327b18baef583d54a73bc
sasikalagithub <sasikalaManthena28@gmail.com>
2022-11-24 10:42:55 +0530

    to check in jenkins

--- src/test/java/PractisePackage/BankerClass.java
+++ src/test/java/PractisePackage/BankerClass.java
@@ -4,7 +4,10 @@ public class BankerClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("This is Banker Class");
+		System.out.println("This is Banker Class1");
+	
+		System.out.println("This is Banker Clas2");
 	}
+	
 
 }
